65e668118b577692b272b0c02fc6d322db3166ab
Nick Giancola <patbenatar@gmail.com>
2020-09-30 17:02:24 -0700

    Fix issue with false expressions

--- lib/rbexy/output_buffer.rb
+++ lib/rbexy/output_buffer.rb
@@ -2,7 +2,7 @@ module Rbexy
   class OutputBuffer < String
     def <<(content)
       value = content.is_a?(Array) ? content.join : content
-      super(value&.to_s || "")
+      super([nil, false].include?(value) ? "" : value.to_s)
     end
   end
 end
