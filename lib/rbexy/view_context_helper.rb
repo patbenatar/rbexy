8b1ce1a9039780e4f48f9ba91e450f7dc67569a5
Nick Giancola <patbenatar@gmail.com>
2020-11-10 09:53:20 -0800

    .

--- lib/rbexy/view_context_helper.rb
+++ lib/rbexy/view_context_helper.rb
@@ -8,8 +8,6 @@ module Rbexy
       @rbexy_context ||= [{}]
     end
 
-    # TODO: this might be an html-injection vulnerabity if an expression is like: {["<h1>Text</h1>"]}
-    # consider checking if all members of an array are html_safe? and only then calling the joined result html_safe
     def rbexy_prep_output(*content)
       return if content.length == 0
       content = content.first
