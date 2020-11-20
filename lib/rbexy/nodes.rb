f22508f5642b7a744b36265d0942d7907bf9f039
Nick Giancola <patbenatar@gmail.com>
2020-11-20 06:32:42 -0800

    Add a list of known html elements

--- lib/rbexy/nodes.rb
+++ lib/rbexy/nodes.rb
@@ -61,6 +61,23 @@ module Rbexy
     class XmlNode
       attr_reader :name, :members, :children
 
+      KNOWN_HTML_ELEMENTS = %w[
+        a abbr acronym address animate animateMotion animateTransform applet area article aside audio b base basefont
+        bdi bdo bgsound big blink blockquote body br button canvas caption center circle cite clipPath code col colgroup
+        color-profile command content data datalist dd defs del desc details dfn dialog dir discard div dl dt element
+        ellipse em embed feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting
+        feDisplacementMap feDistantLight feDropShadow feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage
+        feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence
+        fieldset figcaption figure filter font footer foreignObject form frame frameset g h1 h2 h3 h4 h5 h6 hatch
+        hatchpath head header hgroup hr html i iframe image img input ins isindex kbd keygen label legend li line
+        linearGradient link listing main map mark marker marquee mask menu menuitem mesh meshgradient meshpatch meshrow
+        meta metadata meter mpath multicol nav nextid nobr noembed noframes noscript object ol optgroup option output p
+        param path pattern picture plaintext polygon polyline pre progress q radialGradient rb rect rp rt rtc ruby s
+        samp script section select set shadow slot small solidcolor source spacer span stop strike strong style sub
+        summary sup svg switch symbol table tbody td template text textarea textPath tfoot th thead time title tr track
+        tspan tt u ul unknown use var video view wbr xmp
+      ]
+
       def initialize(name, members, children)
         @name = name
         @members = members || []
