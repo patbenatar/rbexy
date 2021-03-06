1399fc1c1ab972261d8e1fbdd60c8655efa4cc14
Nick Giancola <patbenatar@gmail.com>
2021-03-06 12:00:56 -0800

    WIP more realistic approach to testing cached template changes.. this needed fixing anyway, but doing on ruby3 branch to see if the bad tests are the cause of the ruby3 caching spec failures

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :memory_store
+  config.cache_store = :file_store, Rails.root.join("tmp/cache")
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
