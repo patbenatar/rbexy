a0f92dae075fab1e9e1c23e8477db193d3906005
Nick Giancola <patbenatar@gmail.com>
2021-03-07 07:44:14 -0800

    finish reimplementing caching specs in a more realistic way (rails server stops between warming and making class changes). closes #79

--- spec/dummy/app/controllers/caching_controller.rb
+++ spec/dummy/app/controllers/caching_controller.rb
@@ -1,6 +1,7 @@
 class CachingController < ApplicationController
   def inline; end
   def component; end
+  def component_class; end
   def call_component; end
   def partial_render; end
 end
--- spec/integration/caching/before_changes_spec.rb
+++ spec/integration/caching/before_changes_spec.rb
@@ -1,3 +1,7 @@
+# NOTE: These specs are only meant to run as a part of bin/test,
+# where this file warms the cache, and then after_changes_spec.rb
+# runs next to test source changes against a warm cache.
+
 RSpec.describe CachingController, type: :request, retry: 5 do
   before(:each) { Thread.current[:cache_misses] = 0 }
 
@@ -11,6 +15,11 @@ RSpec.describe CachingController, type: :request, retry: 5 do
   describe "fragments including template sub-components" do
     it "caches the fragment including the sub-component" do
       2.times { get "/caching/component" }
+      expect(Thread.current[:cache_misses]).to eq 1
+    end
+
+    it "caches the fragment including the sub-component (class changes)" do
+      2.times { get "/caching/component_class" }
       expect(Thread.current[:cache_misses]).to eq 1
     end
   end
