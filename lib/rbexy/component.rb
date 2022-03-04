b5db771945a0799e5c58df1cb4abfd9f5f9fe058
Pedro Nascimento <pnascimento@gmail.com>
2022-03-04 18:18:57 +0000

    Add new method being called on Rails 7

--- lib/rbexy/component.rb
+++ lib/rbexy/component.rb
@@ -17,6 +17,10 @@ module Rbexy
       def gsub(*args)
         super.tap { |s| break TemplatePath.new(s) }
       end
+
+      def from(*args)
+        super.tap { |s| break TemplatePath.new(s) }
+      end
     end
 
     class_attribute :component_file_location
