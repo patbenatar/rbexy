0692107efc182ecc73d883e272c3eea670b5909e
Nick Giancola <patbenatar@gmail.com>
2023-11-06 11:32:50 -0800

    Fix Rails 7.1 dev-mode template cache (#95)
    * Make our custom ComponentTemplateResolver view paths work with Rails 7.1 development-mode filesystem template cache
    * Make docker compose setup support debugger

--- lib/rbexy/rails/engine.rb
+++ lib/rbexy/rails/engine.rb
@@ -1,6 +1,31 @@
 require "rbexy/rails"
 require "action_view/dependency_tracker"
 
+# Rails 7.1 implements development-mode template caching in a way that only works with view paths registered as
+# strings with `prepend_view_path`. Since we register our view paths as `ComponentTemplateResolver` instances (which
+# are subclasses of Rails `FileSytemResolver`), we have to monkey-patch Rails to make it think our custom resolvers
+# are file system resolvers and thus should be supported by its caching mechanism.
+if ActionView.version >= Gem::Version.new("7.1")
+  require "action_view/path_registry"
+  module ActionView
+    module PathRegistry
+      class << self
+        alias_method :_original_cast_file_system_resolvers, :cast_file_system_resolvers
+      end
+
+      def self.cast_file_system_resolvers(paths)
+        Array(paths).each do |path|
+          next unless path.is_a?(Rbexy::Rails::ComponentTemplateResolver)
+          @file_system_resolvers[path] ||= path
+          file_system_resolver_hooks.each(&:call)
+        end
+
+        _original_cast_file_system_resolvers(paths)
+      end
+    end
+  end
+end
+
 module Rbexy
   module Rails
     class Engine < ::Rails::Engine
